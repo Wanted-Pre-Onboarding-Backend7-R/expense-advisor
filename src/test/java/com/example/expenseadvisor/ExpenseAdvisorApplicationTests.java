b55597be19571476c5af729ef945f975c5249cc5
junseung <junseung736@gmail.com>
2023-11-15 22:43:38 +0900

    setup #8: Spring Initializer 이용하여 프로젝트 생성

---
+++ src/test/java/com/example/expenseadvisor/ExpenseAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseAdvisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
