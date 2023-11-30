184ba0c7ec6d6a042c7fd5a732e0b97b21486ced
junseung <junseung736@gmail.com>
2023-11-30 13:10:18 +0900

    fix #21: CustomException 에 추가적인 메시지를 담을 수 있도록 수정

--- src/main/java/com/example/expenseadvisor/exception/CustomException.java
+++ src/main/java/com/example/expenseadvisor/exception/CustomException.java
@@ -5,16 +5,30 @@ import lombok.Getter;
 @Getter
 public class CustomException extends RuntimeException {
 
+    private static final String DELIMITER = ": ";
     private final ErrorCode errorCode;
+    private final String combinedMessage;
 
     public CustomException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.combinedMessage = errorCode.getMessage();
     }
 
-    public CustomException(ErrorCode errorCode, Throwable cause) {
-        super(errorCode.getMessage(), cause);
+    public CustomException(ErrorCode errorCode, String additionalMessage) {
+        super(combineMessages(errorCode.getMessage(), additionalMessage));
         this.errorCode = errorCode;
+        this.combinedMessage = combineMessages(errorCode.getMessage(), additionalMessage);
+    }
+
+    public CustomException(ErrorCode errorCode, String additionalMessage, Throwable cause) {
+        super(combineMessages(errorCode.getMessage(), additionalMessage), cause);
+        this.errorCode = errorCode;
+        this.combinedMessage = combineMessages(errorCode.getMessage(), additionalMessage);
+    }
+
+    private static String combineMessages(String msg1, String msg2) {
+        return msg1 + DELIMITER + msg2;
     }
 
 }
